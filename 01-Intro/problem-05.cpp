6d1f7cf673bec3db6a2275f47cde5e4da431841b
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2024-01-28 21:06:33 +0600

    intro level problems

---
+++ 01-Intro/problem-05.cpp
@@ -0,0 +1,18 @@
+/*
+Program that will do the followings:
+a) Declare a variable uninitialized
+b) Declare and initialize a variable in one statement
+c) Declare and initialize multiple variables with different values in one statement
+d) Declare and initialize multiple variables with the same value in one statement
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int number;
+    string name = "zihad";
+    double num1 = 7.54, num2 = 5.330;
+    double num1 = 44.71, num2 = 44.71;
+
+    return 0;
+}
