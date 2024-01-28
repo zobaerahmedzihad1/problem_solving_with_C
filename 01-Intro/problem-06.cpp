4040d0f14c4ec8f2734e38cd9fc52ade7f910c4d
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2024-01-28 21:12:54 +0600

    take input age and print it

---
+++ 01-Intro/problem-06.cpp
@@ -0,0 +1,12 @@
+//Program that will take your age in year(s) as input and print it.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int age;
+    cin>>age;
+    cout<<"my age is : "<<age<<endl;
+
+    return 0;
+}
